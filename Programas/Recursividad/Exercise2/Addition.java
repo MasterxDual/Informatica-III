20412dfdba48bdc427e6144bb1bb5ba6fb3ce38e
Agustin Brambilla <agustinbram@gmail.com>
2023-08-24 16:08:31 -0300

    Exercise2 v1.1

--- Programas/Recursividad/Exercise2/Addition.java
+++ Programas/Recursividad/Exercise2/Addition.java
@@ -1,15 +1,37 @@
 package Exercise2;
 
-public class Addition {
-    public Addition() {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Addition {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Double num;
+
+        System.out.println("Ingrese el numero al que le desea aplicar la suma recursiva");
+        do {
+            num = scan.nextDouble();
+            try {
+                if(num <= 0) {
+                    throw new InputMismatchException("Ha ingresado un numero invalido, vuelva a ingresarlo");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+            }
+        } while(num <= 0);
+        System.out.println(" = " + recursive(num));
+        
+        scan.close();
     }
 
-    public Double recursive(Double num) {
+    public static Double recursive(Double num) throws InputMismatchException{
         if(num == 1) {
+            System.out.print(num);
             return 1.0;
         } else {
-            return num + recursive(num - 1);
+            Double subResult = recursive(num - 1);
+            System.out.print(" + " + num);
+            return num + subResult;
         }
     }
 }
