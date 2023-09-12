41411c08ee1f8cde95f37d0030bca00d6e7780bd
Agustin Brambilla <agustinbram@gmail.com>
2023-09-11 21:51:15 -0300

    Finishing with the program

--- Programas/Practico2/Main.java
+++ Programas/Practico2/Main.java
@@ -1,48 +1,59 @@
 package Practico2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main class");
         OrderingTest orderingTest = new OrderingTest();
+        Scanner scan = new Scanner(System.in);
+        int typeOfObject;
+
+        System.out.println("Ingrese una opcion para utilizar algun tipo de dato: ");
+        System.out.println("1. Integer \n2. Double \n3. String");
+
+        typeOfObject = scan.nextInt();
+
+        System.out.println("Ingrese otra opcion para utilizar algun tipo de ordenamiento: ");
+        System.out.println("1. Insertion sort \n2. Shellsort \n3. Quicksort");
+
+        if(typeOfObject == 1) {
+            orderingTest.testOrdering(Integer.class, scan.nextInt());
+        } else if(typeOfObject == 2) {
+            orderingTest.testOrdering(Double.class, scan.nextInt());
+        } else if(typeOfObject == 3) {
+            orderingTest.testOrdering(String.class, scan.nextInt());
+        }
 
         //Insertion sort for different types
-        orderingTest.testInsertion(Integer.class);
         /*a. 100 elementos: 2 milisegundos
         b. 1000 elementos: 38 milisegundos
         c. 10000 elementos: 714 milisegundos*/
-        orderingTest.testInsertion(Double.class);
         /* a. 100 elementos: 5 milisegundos
         b. 1000 elementos: 119 milisegundos
         c. 10000 elementos: 631 milisegundos*/
-        orderingTest.testInsertion(String.class);
         /* a. 100 elementos: 7 milisegundos
         b. 1000 elementos: 89 milisegundos
         c. 10000 elementos: 971 milisegundos*/
 
         //Shellsort for different types
-        orderingTest.testShellsort(Integer.class);
         /*a. 100 elementos: 1 milisegundos
         b. 1000 elementos: 7 milisegundos
         c. 10000 elementos: 59 milisegundos*/
-        orderingTest.testShellsort(Double.class);
         /*a. 100 elementos: 5 milisegundos
         b. 1000 elementos: 12 milisegundos
         c. 10000 elementos: 138 milisegundos*/
-        orderingTest.testShellsort(String.class);
         /*a. 100 elementos: 2 milisegundos
         b. 1000 elementos: 32 milisegundos
         c. 10000 elementos: 93 milisegundos*/
 
         //Quicksort for different types
-        orderingTest.testQuicksort(Integer.class);
         /*a. 100 elementos: 1 milisegundos
         b. 1000 elementos: 8 milisegundos
         c. 10000 elementos: 32 milisegundos*/
-        orderingTest.testQuicksort(Double.class);
         /*a. 100 elementos: 6 milisegundos
         b. 1000 elementos: 6 milisegundos
         c. 10000 elementos: 109 milisegundos*/
-        orderingTest.testQuicksort(String.class);
         /*a. 100 elementos: 3 milisegundos
         b. 1000 elementos: 7 milisegundos
         c. 10000 elementos: 146 milisegundos*/
--- Programas/Practico2/OrderingTest.java
+++ Programas/Practico2/OrderingTest.java
@@ -1,7 +1,7 @@
 package Practico2;
 
 public class OrderingTest {
-    public <U extends Comparable <U>> void testInsertion(Class <U> type) {
+    public <U extends Comparable <U>> void testOrdering(Class <U> type, int option) {
         Ordering<U> orderer = new Ordering<>(type);
 
         //Carga por teclado la cantidad de elementos que tendrá el array
@@ -10,51 +10,17 @@ public class OrderingTest {
 
         //Cuenta el tiempo en milisegundos actual, antes de empezar a ordenar los datos con el ordenamiento
         long startTime = System.nanoTime();
-        orderer.insertionSort(randomArray);
 
-        //Cuenta el tiempo en milisegundos actual, despues de ordenar los datos con el ordenamiento
-        long endTime = System.nanoTime();
-
-        /*4. Realizar captura de tiempos para cada uno de los algoritmos de ordenamiento para: a. 100 elementos. b. 1000 elementos. c. 10000 elementos.*/
-        //Calcula el tiempo que tardó desde el inicio hasta el final, es decir el tiempo total en ordenar los datos con el ordenamiento
-        long elapsedTimeMillis = (endTime - startTime) / 100000;
-
-        orderer.showArray(randomArray);
-        System.out.println("Tiempo transcurrido: " + elapsedTimeMillis + "milisegundos.");
-    }
-
-    public <U extends Comparable <U>> void testShellsort(Class <U> type) {
-        Ordering<U> orderer = new Ordering<>(type);
-
-        //Carga por teclado la cantidad de elementos que tendrá el array
-        U[] randomArray = orderer.generateArray(orderer.arraySize());
-        orderer.showArray(randomArray);
-
-        //Cuenta el tiempo en milisegundos actual, antes de empezar a ordenar los datos con el ordenamiento
-        long startTime = System.nanoTime();
-        orderer.shell(randomArray, randomArray.length);
-
-        //Cuenta el tiempo en milisegundos actual, despues de ordenar los datos con el ordenamiento
-        long endTime = System.nanoTime();
-
-        /*4. Realizar captura de tiempos para cada uno de los algoritmos de ordenamiento para: a. 100 elementos. b. 1000 elementos. c. 10000 elementos.*/
-        //Calcula el tiempo que tardó desde el inicio hasta el final, es decir el tiempo total en ordenar los datos con el ordenamiento
-        long elapsedTimeMillis = (endTime - startTime) / 100000;
-
-        orderer.showArray(randomArray);
-        System.out.println("Tiempo transcurrido: " + elapsedTimeMillis + "milisegundos.");
-    }
-
-    public <U extends Comparable <U>> void testQuicksort(Class <U> type) {
-        Ordering<U> orderer = new Ordering<>(type);
-
-        //Carga por teclado la cantidad de elementos que tendrá el array
-        U[] randomArray = orderer.generateArray(orderer.arraySize());
-        orderer.showArray(randomArray);
-
-        //Cuenta el tiempo en milisegundos actual, antes de empezar a ordenar los datos con el ordenamiento
-        long startTime = System.nanoTime();
-        orderer.quickSort(randomArray, 0, randomArray.length - 1);
+        if(option == 1) {
+            System.out.println("Insertion sort");
+            orderer.insertionSort(randomArray);
+        } else if(option == 2) {
+            System.out.println("Shellsort");
+            orderer.shell(randomArray, randomArray.length);
+        } else if(option == 3) {
+            System.out.println("Quicksort");
+            orderer.quickSort(randomArray, 0, randomArray.length - 1);
+        }
 
         //Cuenta el tiempo en milisegundos actual, despues de ordenar los datos con el ordenamiento
         long endTime = System.nanoTime();
