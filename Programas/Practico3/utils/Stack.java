e64704ddcb815a7606aa9642e158bc53b13c4307
Agustin Brambilla <agustinbram@gmail.com>
2023-09-12 20:29:04 -0300

    Adding commentary

--- Programas/Practico3/utils/Stack.java
+++ Programas/Practico3/utils/Stack.java
@@ -4,6 +4,14 @@ import Practico3.exceptions.StackIsEmptyException;
 import Practico3.exceptions.StackIsFullException;
 
 public class Stack <T> {
+    /*1. Implementación de una Pila:
+    a. Crea una clase que represente una pila utilizando un arreglo.
+    b. Implementa métodos para push (agregar un elemento), pop (eliminar
+    el elemento en la cima) y top (ver el elemento en la cima sin
+    eliminarlo).
+    c. Prueba tu implementación utilizando diferentes operaciones y
+    elementos. */
+    
     /**
     * Indice de array en donde tenemos el último elemento.
     */
