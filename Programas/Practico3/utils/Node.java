a105fa2a4720543cb0b81c79b3829dc9f9a00598
Agustin Brambilla <agustinbram@gmail.com>
2023-09-24 16:54:22 -0300

    setters and getters of Node class

--- Programas/Practico3/utils/Node.java
+++ Programas/Practico3/utils/Node.java
@@ -8,4 +8,21 @@ public class Node<T> {
         this.data = data;
         this.next = null;
     }
+    
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
 }
