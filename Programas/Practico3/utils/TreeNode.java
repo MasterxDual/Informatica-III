693585bc97233d70ab127340f90c37a273846f8e
Agustin Brambilla <agustinbram@gmail.com>
2023-10-03 17:37:01 -0300

    Getting started with binary trees

---
+++ Programas/Practico3/utils/TreeNode.java
@@ -0,0 +1,62 @@
+package Practico3.utils;
+
+public class TreeNode <T> {
+    protected T value;
+    protected TreeNode<T> leftNode;
+    protected TreeNode<T> rightNode;
+
+    public TreeNode(T value) {
+        this.value = value;
+        this.leftNode = this.rightNode = null;
+    }
+    
+    public TreeNode(T value, TreeNode<T> leftNode, TreeNode<T> rightNode) {
+        this.value = value;
+        this.leftNode = leftNode;
+        this.rightNode = rightNode;
+    }
+
+    /**
+     * Returns the value of the tree node 
+     * @return value from tree node
+     */
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    /**
+     * Returns the sub-tree left node (sub arbol izquierdo) 
+     * @return left node sub-tree
+     */
+    public TreeNode<T> getLeftNode() {
+        return leftNode;
+    }
+
+    /**
+     * Sets the left branch of the tree (rama izquierda)
+     * @param leftNode 
+     */
+    public void setLeftNode(TreeNode<T> leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    /**
+     * Returns the sub-tree right node (sub arbol derecho) 
+     * @return rigth node sub-tree
+     */
+    public TreeNode<T> getRightNode() {
+        return rightNode;
+    }
+    
+    /**
+     * Sets the right branch of the tree (rama derecha)
+     * @param rightNode
+     */
+    public void setRightNode(TreeNode<T> rightNode) {
+        this.rightNode = rightNode;
+    }
+}
