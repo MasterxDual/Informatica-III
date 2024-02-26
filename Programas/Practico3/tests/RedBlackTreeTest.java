67ee680329e2f7959b02f145758f27f15c019aa8
Agustin Brambilla <agustinbram@gmail.com>
2024-02-26 16:18:14 -0300

    Adding comments to RedBlackTreeTest methods

--- Programas/Practico3/tests/RedBlackTreeTest.java
+++ Programas/Practico3/tests/RedBlackTreeTest.java
@@ -3,6 +3,10 @@ package Practico3.tests;
 import Practico3.utils.RedBlackTree;
 
 public class RedBlackTreeTest <T extends Comparable<T>> {
+    /**
+     * Creates a red-black tree, inserting numbers 10, 20, 30, 40, 50, 60, and 70 respectively.
+     * @return the red-black tree created.
+     */
     public RedBlackTree<Integer> createFirstRedBlackTree() {
         RedBlackTree<Integer> redBlackTree = new RedBlackTree<>(Integer.class);
 
@@ -17,6 +21,11 @@ public class RedBlackTreeTest <T extends Comparable<T>> {
         return redBlackTree;
     }
 
+    /**
+     * Creates another red-black tree, inserting the same numbers of first red-black tree, with the
+     * difference that it removes the element with value '30'.
+     * @return the red-black tree created.
+     */
     public RedBlackTree<Integer> createSecondRedBlackTree() {
         RedBlackTree<Integer> redBlackTree = new RedBlackTree<>(Integer.class);
 
@@ -32,6 +41,12 @@ public class RedBlackTreeTest <T extends Comparable<T>> {
         return redBlackTree;
     }
 
+    /**
+     * Created a third red-black tree, inserting values 80, 40, 90, 100 and 20. Then
+     * rotates it right in the left node of root node, then rotates it left in the
+     * right node of root node and finally inserts value 85.
+     * @return the red-black tree created.
+     */
     public RedBlackTree<Integer> createThirdRedBlackTree() {
         RedBlackTree<Integer> redBlackTree = new RedBlackTree<>(Integer.class);
 
