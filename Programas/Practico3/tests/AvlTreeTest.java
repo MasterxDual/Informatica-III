e8afdd3933038280e55509c411bd8fea4f506ce7
Agustin Brambilla <agustinbram@gmail.com>
2024-02-26 16:21:00 -0300

    Add comments to AvlTreeTest methods

--- Programas/Practico3/tests/AvlTreeTest.java
+++ Programas/Practico3/tests/AvlTreeTest.java
@@ -4,6 +4,10 @@ import Practico3.utils.AVLNode;
 import Practico3.utils.AVLTree;
 
 public class AvlTreeTest <U extends Comparable<U>> {
+    /**
+     * Creates an avl-tree, inserting values 10, 100, 20, 80, 40 and 70.
+     * @return the avl-tree created.
+     */
     public AVLTree<Integer> createFirstAvlTree() {
         AVLNode<Integer> rootNode = new AVLNode<>(10);
         AVLTree<Integer> avlTree = new AVLTree<>(rootNode);
@@ -16,7 +20,11 @@ public class AvlTreeTest <U extends Comparable<U>> {
 
         return avlTree;
     }
-
+    
+    /**
+     * Creates an avl-tree, inserting values 5, 10, 20, 30, 40, 50 and 60.
+     * @return the avl-tree created.
+     */
     public AVLTree<Integer> createSecondAvlTree() {
         AVLNode<Integer> rootNode = new AVLNode<>(5);
         AVLTree<Integer> avlTree = new AVLTree<>(rootNode);
@@ -31,6 +39,10 @@ public class AvlTreeTest <U extends Comparable<U>> {
         return avlTree;
     }
 
+    /**
+     * Creates an avl-tree, inserting values 4, 2, 30, 1, 10, 5, 20, 50, 40 and 60.
+     * @return the avl-tree created.
+     */
     public AVLTree<Integer> createThirdAvlTree() {
         AVLNode<Integer> rootNode = new AVLNode<>(4);
         AVLTree<Integer> avlTree = new AVLTree<>(rootNode);
